e639809f452d7f5e6d5f08a9a487a2635acd4c91
kJanya <129998932+kJanya@users.noreply.github.com>
2024-11-29 15:17:06 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">ZEESHU</div>
+          <div className="right">
+            <p>New-Delhi , India</p>
+            <p>Open: 09:00 AM - 11:00 PM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed by Team</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Team</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
